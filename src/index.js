ade68aa3263a5fec4c46fa62386054f8bc56be2f
andrei-kryvetski <kryvetskistud@gmail.com>
2021-07-17 11:31:32 +0300

    refactor: remove less comments

--- src/index.js
+++ src/index.js
@@ -37,58 +37,6 @@ const MORSE_TABLE = {
   '-----':  '0',
 };
 
-// function decode(expr) {
-//   let result = []
-//   let result2 = []
-//   const splitedExpr = expr.split('')
-//   const wordLength = 10
-//   const quantityOfWords = expr.length / wordLength
-//   console.log(quantityOfWords, ' - quantityOfWords') 
-
-//   function some(startIndex, endIndex) {
-//     result = []
-//     if (startIndex === expr.length) return
-//     // if (startIndex === 150) return
-//     for (let i = startIndex; i < endIndex; i = i + 2) {
-//       if (splitedExpr[i] != '**') {
-//         result.push(splitedExpr[i] + splitedExpr[i + 1])
-//       }
-//       if (result.length === 0) {
-//         result.push(' ')
-//       }
-      
-//     }
-
-//     for (let i = 0; i < result.length; i++) {
-//       if (result[i] === '10') {
-//         result[i] = '.'
-//       } else if (result[i] === '11') {
-//         result[i] = '-'
-//       } 
-//     }
-//     console.log(result)
-//     const joinedResult = result.join('')
-//     const filtered = joinedResult.split('').filter((letter) => {return letter.indexOf('0')} )
-
-//     for (key in MORSE_TABLE) {
-//       if (key === filtered.join('')) {
-//         result2.push(MORSE_TABLE[key])
-//       }
-//       if (filtered.join('') === ' ') {
-//         result2.push(' ')
-//       }
-//     }
-//     some(startIndex + 10, endIndex + 10)
-//     // console.log(result)
-//     // console.log(joinedResult, ' - joinedResult')
-//     // console.log(filtered, ' - filtered')
-//     // console.log(result2.join(''))
-//   }
-//   some(0, 10)
-//   console.log(result2.join(''))
-//   return result2.join('')
-// }
-
 function decode(expr) {
   const splitedExpr = expr.split('');
   let stash = [];
@@ -120,9 +68,10 @@ function decode(expr) {
   }
 
   recursion(0, 10)
-  
+
   return result.join('')
 }
+
 module.exports = {
   decode
 }
